07259f2445244563ce9f3e3ccca4eb4a146fcd07
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-11-01 14:48:25 +0100

    https://github.com/jabrena/ev3dev-lang-java/issues/7

---
+++ library/src/main/java/org/ev3dev/base/App.java
@@ -0,0 +1,13 @@
+package org.ev3dev.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
