407e68e30bdcc187d727d862faf05f8b162846a4
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-12-09 21:08:37 +0100

    Adding temporal examples. Later, I will move to an isolated Java project https://github.com/jabrena/ev3dev-lang-java/issues/53

---
+++ library/src/main/java/ev3dev/examples/Test5.java
@@ -0,0 +1,20 @@
+package ev3dev.examples;
+
+import java.io.File;
+
+import ev3dev.hardware.Sound;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File file = new File("/home/cncsounds/commander.wav");
+		Sound.setVolume(100);
+		Sound.playSample(file);
+		Sound.beep();
+		Sound.twoBeeps();
+		Sound.playTone(300, 50000, 100);
+		System.out.println(Sound.getVolume());
+	}
+
+}
