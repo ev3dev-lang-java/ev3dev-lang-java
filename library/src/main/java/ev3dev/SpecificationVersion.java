791a823f949a42b7ffa086fd081c1d26aea465d9
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-12-13 13:53:39 +0100

    https://github.com/jabrena/ev3dev-lang-java/issues/6

---
+++ library/src/main/java/ev3dev/SpecificationVersion.java
@@ -0,0 +1,12 @@
+package ev3dev;
+
+public class SpecificationVersion {
+
+	//~autogen spec_version
+	
+	public static final String spec_version = "spec: 0.9.3-pre-r2, kernel: v3.16.7-ckt16-7-ev3dev-ev3";
+
+	//~autogen
+	
+}
+
