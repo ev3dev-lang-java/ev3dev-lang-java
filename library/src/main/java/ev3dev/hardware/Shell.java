3860aba83c3cd620128de7c7432ae7ea518b16f0
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-12-28 17:19:53 +0100

    https://github.com/jabrena/ev3dev-lang-java/issues/70

--- library/src/main/java/ev3dev/hardware/Shell.java
+++ library/src/main/java/ev3dev/hardware/Shell.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Shell {
-
+	
 	public static String execute(final String command) {
 
 		StringBuffer output = new StringBuffer();
@@ -15,12 +15,38 @@ public class Shell {
 			p.waitFor();
 			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line = "";			
 			
-			while ((line = reader.readLine()) != null) {
+			String line = "";			
+			while ((line = reader.readLine())!= null) {
 				output.append(line + "\n");
 			}
+			reader.close();
 		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+		return output.toString();
+	}
+	
+	public static String execute(final String[] command) {
+
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			
+			String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 
