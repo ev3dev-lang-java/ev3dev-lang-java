62908728a8115c2013190ff934fbc8c3a66b9e75
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-06-03 12:12:26 +0100

    LCD Refactoring Adding utility class: JarResource

--- src/main/java/ev3dev/actuators/ev3/lcd/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Access to the EV3 LCD
- */
-package ev3dev.actuators.ev3.lcd;
---
+++ src/main/java/ev3dev/actuators/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Manage devices which interact with the world like LCD &amp; Sound.
+ */
+package ev3dev.actuators;
