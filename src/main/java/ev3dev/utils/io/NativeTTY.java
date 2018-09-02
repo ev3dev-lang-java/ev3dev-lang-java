403d2576b813d42d37969312d4254dff7b73bfa9
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-09-02 15:31:58 +0200

    Add documentation for NativeTTY

--- src/main/java/ev3dev/utils/io/NativeTTY.java
+++ src/main/java/ev3dev/utils/io/NativeTTY.java
@@ -10,16 +10,41 @@ import java.util.List;
 
 import static ev3dev.utils.io.NativeConstants.*;
 
+/**
+ * Wrapper for basic actions on Linux VT/TTY
+ *
+ * @author Jakub Vaněk
+ * @since 2.4.7
+ */
 public class NativeTTY extends NativeDevice {
 
+    /**
+     * Initialize new TTY.
+     *
+     * @param dname Path to TTY device.
+     * @throws LastErrorException when the operation fails.
+     */
     public NativeTTY(String dname) throws LastErrorException {
         super(dname, NativeConstants.O_RDWR);
     }
 
+    /**
+     * Initialize new TTY.
+     *
+     * @param dname Path to TTY device.
+     * @param flags Opening mode, e.g. read, write or both.
+     * @throws LastErrorException when the operation fails.
+     */
     public NativeTTY(String dname, int flags) throws LastErrorException {
         super(dname, flags);
     }
 
+    /**
+     * Get current TTY mode. TTY mode is mostly about VT switching.
+     *
+     * @return TTY mode.
+     * @throws LastErrorException when the operation fails.
+     */
     public vt_mode getVTmode() throws LastErrorException {
         vt_mode mode = new vt_mode();
         super.ioctl(VT_GETMODE, mode.getPointer());
@@ -27,11 +52,23 @@ public class NativeTTY extends NativeDevice {
         return mode;
     }
 
+    /**
+     * Set current TTY mode. TTY mode is mostly about VT switching.
+     *
+     * @param mode TTY mode.
+     * @throws LastErrorException when the operation fails.
+     */
     public void setVTmode(vt_mode mode) throws LastErrorException {
         mode.write();
         super.ioctl(VT_SETMODE, mode.getPointer());
     }
 
+    /**
+     * Get current TTY state.
+     *
+     * @return TTY state.
+     * @throws LastErrorException when the operation fails.
+     */
     public vt_stat getVTstate() throws LastErrorException {
         vt_stat stat = new vt_stat();
         super.ioctl(VT_GETSTATE, stat.getPointer());
@@ -39,24 +76,51 @@ public class NativeTTY extends NativeDevice {
         return stat;
     }
 
+    /**
+     * Get current keyboard mode.
+     *
+     * @return Keyboard mode (raw, transformed or off) - K_* constants.
+     * @throws LastErrorException when the operation fails.
+     */
     public int getKeyboardMode() throws LastErrorException {
         IntByReference kbd = new IntByReference(0);
         super.ioctl(KDGKBMODE, kbd);
         return kbd.getValue();
     }
 
+    /**
+     * Set keyboard mode.
+     *
+     * @param mode Keyboard mode (raw, transformed or off) - K_* constants.
+     * @throws LastErrorException when the operation fails.
+     */
     public void setKeyboardMode(int mode) throws LastErrorException {
         super.ioctl(KDSKBMODE, mode);
     }
 
+    /**
+     * Set console mode.
+     *
+     * @param mode Console mode - graphics or text mode - KD_* constants.
+     * @throws LastErrorException when the operation fails.
+     */
     public void setConsoleMode(int mode) throws LastErrorException {
         super.ioctl(KDSETMODE, mode);
     }
 
+    /**
+     * Signal VT switch to the kernel.
+     *
+     * @param mode VT switching signal - VT_* constants.
+     * @throws LastErrorException when the operation fails.
+     */
     public void signalSwitch(int mode) throws LastErrorException {
         super.ioctl(VT_RELDISP, mode);
     }
 
+    /**
+     * Info about an active VT.
+     */
     public static class vt_stat extends Structure {
         public short v_active; /* active vt */
         public short v_signal; /* signal to send */
@@ -76,6 +140,9 @@ public class NativeTTY extends NativeDevice {
         }
     }
 
+    /**
+     * Info about VT configuration.
+     */
     public static class vt_mode extends Structure {
         public byte mode;        /* vt mode */
         public byte waitv;        /* if set, hang on writes if not active */
