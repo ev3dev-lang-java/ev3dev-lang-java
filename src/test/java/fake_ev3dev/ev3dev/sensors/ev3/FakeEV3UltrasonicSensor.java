bba052d41dd90a7ee38e433b3db8d2cd1bbab026
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-10-29 22:51:15 +0100

    Refactor fake sensors - lower code duplication

--- src/test/java/fake_ev3dev/ev3dev/sensors/ev3/FakeEV3UltrasonicSensor.java
+++ src/test/java/fake_ev3dev/ev3dev/sensors/ev3/FakeEV3UltrasonicSensor.java
@@ -1,12 +1,9 @@
 package fake_ev3dev.ev3dev.sensors.ev3;
 
 import ev3dev.hardware.EV3DevPlatform;
-import ev3dev.utils.Sysfs;
 import fake_ev3dev.ev3dev.sensors.FakeLegoSensor;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FakeEV3UltrasonicSensor extends FakeLegoSensor {
@@ -15,31 +12,9 @@ public class FakeEV3UltrasonicSensor extends FakeLegoSensor {
         super(ev3DevPlatform);
 
         populateValues(Arrays.asList(20));
-
-        Path mode = Paths.get(
-                EV3DEV_FAKE_SYSTEM_PATH + "/" +
-                        LEGO_SENSOR_PATH + "/" +
-                        SENSOR1 + "/" +
-                        SENSOR_MODE);
-
-        createFile(mode);
     }
 
     public void setListenMode() throws IOException {
-
-        Path value0 = Paths.get(
-                EV3DEV_FAKE_SYSTEM_PATH + "/" +
-                        LEGO_SENSOR_PATH + "/" +
-                        SENSOR1 + "/" +
-                        VALUE0);
-        createFile(value0, String.valueOf(0));
-
-    }
-
-    public String getCurrentMode() {
-        return Sysfs.readString(EV3DEV_FAKE_SYSTEM_PATH + "/" +
-                LEGO_SENSOR_PATH + "/" +
-                SENSOR1 + "/" +
-                SENSOR_MODE);
+        this.writeFileDirect(String.valueOf(0), SENSOR1_BASE, "value0");
     }
 }
--- src/test/java/fake_ev3dev/ev3dev/sensors/mindsensors/FakeAbsolutIMUSensor.java
+++ src/test/java/fake_ev3dev/ev3dev/sensors/mindsensors/FakeAbsolutIMUSensor.java
@@ -4,8 +4,6 @@ import ev3dev.hardware.EV3DevPlatform;
 import fake_ev3dev.ev3dev.sensors.FakeLegoSensor;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class FakeAbsolutIMUSensor extends FakeLegoSensor {
@@ -15,22 +13,6 @@ public class FakeAbsolutIMUSensor extends FakeLegoSensor {
         super(ev3DevPlatform);
 
         populateValues(Arrays.asList(2, 100, 200));
-
-        //TODO It is possible to generalize a bit more
-        Path mode = Paths.get(
-                EV3DEV_FAKE_SYSTEM_PATH + "/" +
-                        LEGO_SENSOR_PATH + "/" +
-                        SENSOR1 + "/" +
-                        SENSOR_MODE);
-        createFile(mode);
-
-        Path command = Paths.get(
-                EV3DEV_FAKE_SYSTEM_PATH + "/" +
-                        LEGO_SENSOR_PATH + "/" +
-                        SENSOR1 + "/" +
-                        SENSOR_COMMAND);
-        createFile(command);
-
     }
 
 }
